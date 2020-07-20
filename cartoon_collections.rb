0958ca7d4e8a268339d74ca44a1e206898b8233e
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 01:58:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,3 +11,14 @@ def long_planeteer_calls(words)
   words.any? { |x| x.length > 4 }
 end
 
+def find_the_cheese(list)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if list.include?("cheddar")
+    "cheddar"
+  elsif list.include?("gouda")
+    "gouda"
+  elsif list.include?("camembert")
+      "camembert"
+  else nil
+  end    
+end
